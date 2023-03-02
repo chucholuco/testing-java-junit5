5182e1c0bc75107fc761571c13052a8c50114183
chucholuco <chucholuco@gmail.com>
2023-03-01 23:57:14 -0600

    adding message (#2)
    * adding message
    
    * adding comment

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetSDJpaServiceIT.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetSDJpaServiceIT.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Testing
+ */
 @ExtendWith(TimingExtension.class)
 class PetSDJpaServiceIT {
 
