e54c7470914732b69a35f5dca723fc8d27eca37d
Jesus Tapia <chucholuco@gmail.com>
2023-03-01 23:55:29 -0600

    adding comment

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetSDJpaServiceIT.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetSDJpaServiceIT.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- *
+ * Testing
  */
 @ExtendWith(TimingExtension.class)
 class PetSDJpaServiceIT {
