7e836e1be93bfe34ac6fc74f1aaead86b52c355f
Jesus Tapia <chucholuco@gmail.com>
2023-03-13 19:07:48 -0600

    new comments

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
@@ -7,7 +7,9 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
+/**
+ *
+ */
 class OwnerSDJpaServiceTest {
 
     OwnerSDJpaService service;
--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetSDJpaServiceIT.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetSDJpaServiceIT.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Testing
+ * Testing 2
  */
 @ExtendWith(TimingExtension.class)
 class PetSDJpaServiceIT {
